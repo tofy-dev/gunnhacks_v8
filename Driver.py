ad26cf901a7aad48d097bd7b0c8fc45ca8093b8e
tofy <80428886+tofy-dev@users.noreply.github.com>
2022-02-05 14:28:41 -0800

    fixed driver.py

--- Driver.py
+++ Driver.py
@@ -1,15 +1,26 @@
 import pygame
 from pygame.locals import *
+pygame.display.set_caption("AstBlast")
 
+# window information
 WIDTH, HEIGHT = 900, 500
-WIN = pygame.display.set_mode((WIDTH, HEIGHT));
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# colors
+BLACK = (0, 0, 0)
+
+def draw_window():
+    WIN.fill(BLACK)
+    pygame.display.update()
 
 def main():
-    run = True;
+    run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        
+        draw_window()
     
     pygame.quit()
 
