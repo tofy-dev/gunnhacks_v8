5a424b10d4c511f277b2251c78e80275d094a7b4
coder2003lucky <88560765+coder2003lucky@users.noreply.github.com>
2022-02-05 14:50:26 -0800

    Create Movable.py

---
+++ Movable.py
@@ -0,0 +1,8 @@
+class Movable:
+
+    # dir is the angle the player is facing in radians
+    dir = 0
+
+    # speed is the number of pixels-ish that the player will move forward
+    speed = 2
+    def move(self):
