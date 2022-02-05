1cc08a7a30e7137c3dc86fd5d2a950ad5898e7a3
tofy <80428886+tofy-dev@users.noreply.github.com>
2022-02-05 14:49:00 -0800

    display player on screen

--- Driver.py
+++ Driver.py
@@ -1,21 +1,29 @@
 import pygame
+import os
 from pygame.locals import *
 pygame.display.set_caption("AstBlast")
 
-# window information
+# game information
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+FPS = 60
 
-# colors
+# visuals
 BLACK = (0, 0, 0)
+SPACESHIP_IMG = pygame.image.load(os.path.join('sprites', 'player.png'))
 
+# updates what's displayed on the screen
 def draw_window():
     WIN.fill(BLACK)
+    WIN.blit(SPACESHIP_IMG, (WIDTH/2, HEIGHT/2))
     pygame.display.update()
 
+# runs the program
 def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
