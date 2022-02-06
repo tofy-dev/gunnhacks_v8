c701474955282466f7f1052c5e53c9f002172e6e
B3lla123 <42219102+B3lla123@users.noreply.github.com>
2022-02-05 17:33:18 -0800

    asteroids filled in

--- Asteroids.java
+++ Asteroids.java
@@ -1,3 +1,24 @@
-public class Asteroids{
+public class Asteroids extends Movable{
+	
+	int radius
+	public Asteroids(int tx, int ty, int tdx, int tdy, int tradius) {
+		super(tx,ty,tdx,tdy);
+		radius = tradius
+	}
+	
+	public void setRadius(int z) radius = z;
+	
+	public void getRadius() return radius;
+
+	public void notifyIfHit(){
+
+	}
+
+	public void getType() return 3;
+
+	public void remove(){
+
+	}
+	
 	
 }
