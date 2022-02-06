97e583a5fe7579afeb5a125e6713169dbb8e95cf
B3lla123 <42219102+B3lla123@users.noreply.github.com>
2022-02-06 11:50:59 -0800

    fixed the sprites

--- Player.java
+++ Player.java
@@ -287,13 +287,13 @@ public class Player extends Movable implements KeyListener {
 			}
 			int nX = getX() + getWidth() / 2 + (int) (r * Math.cos(tDir));
 			int nY = getY() + getHeight() / 2 + (int) (r * Math.sin(tDir));
-			Driver.getLasers().add(new Laser(nX, nY, 20, 5, dir, 10, getPanel()));
+			Driver.getLasers().add(new Laser(nX, nY, 10, 10, dir, 10, getPanel()));
 			laserCooldown = System.currentTimeMillis();
 		}
 	}
 	
 	public void shootMissile() {
-		if (System.currentTimeMillis() - missileCooldown >= 1000) {
+		if (System.currentTimeMillis() - missileCooldown >= 5000) {
 			int r = getWidth() / 2;
 
 			double tDir = dir - Math.PI / 2;
@@ -303,7 +303,7 @@ public class Player extends Movable implements KeyListener {
 			}
 			int nX = getX() + getWidth() / 2 + (int) (r * Math.cos(tDir));
 			int nY = getY() + getHeight() / 2 + (int) (r * Math.sin(tDir));
-			Driver.getLasers().add(new Missile(nX, nY, 20, 5, dir, 10, getPanel()));
+			Driver.getLasers().add(new Missile(nX, nY, 10, 10, dir, 10, getPanel()));
 			missileCooldown = System.currentTimeMillis();
 		}
 	}
