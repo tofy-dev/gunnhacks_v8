bc52a3b4f1ca4eba221cd301a073bca731a6ddde
B3lla123 <42219102+B3lla123@users.noreply.github.com>
2022-02-05 17:07:21 -0800

    filled in Movable

--- Movable.java
+++ Movable.java
@@ -1,3 +1,70 @@
 public class Movable{
 	
+	// Not sure how to add the image and the Jpanel
+	int x;
+	int y;
+	int dx;
+	int dy;
+	public Movable(int tx, int ty, int tdx, int tdy) {
+		x = tx;
+		y = ty;
+		dx = tdx;
+		dy = tdy;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int z) {
+		x = z;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int z) {
+		y = z;
+	}
+	
+	public void setDx(int z) {
+		dx = z;
+	}
+	
+	public int getDx() {
+		return dx;
+	}
+	
+	public void setDy(int z) {
+		dy = z;
+	}
+	
+	public int getDy() {
+		return dy;
+	}
+	
+	public int getType() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void notifyIfHit() {
+		
+	}
+	
+	public void remove() {
+		
+	}
+	
+	public void move() {
+		x += dx;
+		y += dy;
+	}
+	
+	
+	
 }
